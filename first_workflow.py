dc89ea309ae0b2b7cb636541d4b84e055f533e8c
ewan <ewan@T10-LT-1333.centre4.local>
2025-04-29 14:25:14 +0100

    intial commit

---
+++ first_workflow.py
@@ -0,0 +1,12 @@
+def adder(a, b):
+    """
+    Adds two numbers together.
+    
+    Parameters:
+    a (int): The first number.
+    b (int): The second number.
+    
+    Returns:
+    int: The sum of a and b.
+    """
+    return a + b
